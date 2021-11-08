e7643709f022f3fc015b0d3240ba701cbe699978
your name here <mmakhambetov@yandex.ru>
2021-11-08 11:24:27 +0400

    Add the Park List page

--- routes.js
+++ routes.js
@@ -1,12 +1,20 @@
 const express = require('express');
 
 const { environment } = require('./config/index');
+const db = require('./db/models');
 
 const router = express.Router();
+
+const asyncHandler = handler => (req, res, next) => handler(req, res, next).catch(next);
 
 router.get('/', (req, res) => {
   res.render('index', { title: 'Home' });
 });
+
+router.get('/parks', asyncHandler(async (req, res, next) => {
+  const parks = await db.Park.findAll({ order: [['parkName', 'ASC']] });
+  res.render('park-list', { title: 'Parks', parks });
+}));
 
 if (environment !== 'production') {
   router.get('/error-test', () => {
